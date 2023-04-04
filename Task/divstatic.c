38aa846d8439ad1d1ffa8ff122826ce59d4bb91b
devangflutter <devangmalaviya111@gmail.com>
2023-04-04 14:36:55 +0530

    commit
    complate

---
+++ Task/divstatic.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+main()
+{
+	int num=50,num1=25;//static
+	printf("This is addtion of num and num1 :%d \n",num/num1);//assing tow int
+}
---
+++ Task/substatic.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+main()
+{
+	int num=10,num1=30;//static
+	printf("This is addtion of num and num1 :%d \n",num-num1);//assing tow int
+}
